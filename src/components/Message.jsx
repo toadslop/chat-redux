ecc2449d1de4c59909b1d7ae300c6c9270de3672
Brian Heise <bnheise@gmail.com>
2020-03-30 12:08:47 +0900

    names now display with a color

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -2,11 +2,30 @@
 import React, { Component } from 'react';
 
 export default class Message extends Component {
+  hashCode = (str) => { // java String#hashCode
+    let hash = 0;
+    for (let i = 0; i < str.length; i++) {
+      hash = str.charCodeAt(i) + ((hash << 5) - hash);
+    }
+    return hash;
+  }
+
+  intToRGB = (i) => {
+    const c = (i & 0x00FFFFFF)
+      .toString(16)
+      .toUpperCase();
+    return "00000".substring(0, 6 - c.length) + c;
+  }
+
   render() {
+    const { message } = this.props;
     return (
       <div>
-        <p>{this.props.message.author} - {this.props.message.created_at}</p>
-        <p>{this.props.message.content}</p>
+        <div className="message-heading">
+          <p className="user-name" style={{ color: `#${this.intToRGB(this.hashCode(message.author))}` }}>{message.author}</p>
+          <p>&nbsp;- {message.created_at}</p>
+        </div>
+        <p>{message.content}</p>
       </div>
     );
   }
